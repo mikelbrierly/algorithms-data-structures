3bc6bdc36dcdfae40f9ca9f0836a2f8eb6cbbe49
mikelbrierly <mikel@hey.com>
2023-10-25 16:44:18 -0600

    added grokking notes

---
+++ grokking/BigO.js
@@ -0,0 +1,3 @@
+// Logarithms
+
+// logarithms (log) are the INVERSE of exponentials.
--- grokking/binarySearch.js
+++ grokking/binarySearch.js
@@ -1,1 +1,6 @@
-// Binary search algorithm notes
+// Binary Search
+
+// Binary search is an algorithm that takes a SORTED input and returns the LOCATION
+// of the item being searched for. (works like git bisect)
+
+// 'Binary' because it's either this or that. One or the other.
