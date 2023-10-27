ba9fb1ca7a223195bd9e5ca57dd402398f1aa978
mikelbrierly <mikel@hey.com>
2023-10-27 12:40:07 -0600

    grokking notes

--- grokking/BigO.js
+++ grokking/BigO.js
@@ -1,3 +1,9 @@
+// Big O
+
+// Algorithm speed isn't measured in time, but in growth of operations. (run time increases as input increases)
+// Run time is described in Big O
+//O(log n) is much faster than O(n)
+
 // Logarithms
 
 // logarithms (log) are the INVERSE of exponentials.
--- grokking/binarySearch.js
+++ grokking/binarySearch.js
@@ -3,4 +3,30 @@
 // Binary search is an algorithm that takes a SORTED input and returns the LOCATION
 // of the item being searched for. (works like git bisect)
 
-// 'Binary' because it's either this or that. One or the other.
+// 'Binary' because it's either this side of the array or the other side of the array. One or the other.
+
+const arr = [1, 2, 3, 5, 6, 9, 100];
+
+const binarySearch = (sorted, item) => {
+  let low = 0;
+  let high = sorted.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    
+    let guess = sorted[mid];
+
+    if (guess === item) return mid;
+    if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
+};
+
+console.log(binarySearch(arr, 3));
+
+// Complexity
+// 0(log n);
