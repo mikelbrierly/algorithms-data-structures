fcdcf7d02d1904473d0f45240a46c73fd190e62c
mikelbrierly <mikel@hey.com>
2023-10-31 10:31:03 -0600

    selection sort

--- grokking/selectionSort.js
+++ grokking/selectionSort.js
@@ -12,48 +12,26 @@
 
 const inputArr = [600, 4, 44, 2, 949, 44, 31, 92, 233];
 
-// const findSmallest = (arr) => {
-//   let sm = arr[0];
-//   let smIndex = 0;
+const selectionSort = (a, mutate = false) => {
+  let arr = a;
+  if (mutate) arr = [...a];
 
-//   for (i in arr) {
-//     if (arr[i] < sm) {
-//       sm = arr[i];
-//       smIndex = i;
-//     }
-//   }
-//   return smIndex;
-// };
-
-// // console.log(findSmallest(inputArr));
-
-// const selectionSort = (arr) => {
-//   const newArr = [];
-//   arr.forEach((_a) => {
-//     let sm = findSmallest(arr);
-//     newArr.push(...arr.splice(sm, 1));
-//   });
-
-//   return newArr;
-// };
-
-const selectionSort = (arr) => {
-  const newArr = [...arr];
   // iterate over array
-  for (let i = 0; i <= newArr.length - 1; i++) {
-    let minIndex = i;
-    for (let j = i; j <= newArr.length; j++) {
-      if (newArr[j] < newArr[minIndex]) minIndex = j;
+  for (let i = 0; i <= arr.length - 1; i++) {
+    let smallestIndex = i;
+    for (let j = i; j <= arr.length; j++) {
+      if (arr[j] < arr[smallestIndex]) smallestIndex = j;
     }
 
-    // const temp = newArr[i];
-    // newArr[i] = newArr[minIndex];
-    // newArr[minIndex] = temp;
+    // const temp = arr[i];
+    // arr[i] = arr[smallestIndex];
+    // arr[smallestIndex] = temp;
 
-    // with newArray destructuring
-    [newArr[i], newArr[minIndex]] = [newArr[minIndex], newArr[i]];
+    // with array destructuring
+    console.log(([arr[i], arr[smallestIndex]] = [arr[smallestIndex], arr[i]]));
+    [arr[i], arr[smallestIndex]] = [arr[smallestIndex], arr[i]];
   }
-  return newArr;
+  return arr;
 };
 
 console.log(selectionSort(inputArr));
