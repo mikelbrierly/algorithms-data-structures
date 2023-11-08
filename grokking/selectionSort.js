4c93769d4c471d6b5f1830cecc915b895ae9e545
mikelbrierly <mikel@hey.com>
2023-11-08 13:36:34 -0700

    updated selection sort with an example that I wrote, without array destructuring

--- grokking/selectionSort.js
+++ grokking/selectionSort.js
@@ -12,30 +12,22 @@
 
 const inputArr = [600, 4, 44, 2, 949, 44, 31, 92, 233];
 
-const selectionSort = (a, mutate = false) => {
-  let arr = a;
-  if (mutate) arr = [...a];
-
-  // iterate over array
-  for (let i = 0; i <= arr.length - 1; i++) {
-    let smallestIndex = i;
-    for (let j = i; j <= arr.length; j++) {
-      if (arr[j] < arr[smallestIndex]) smallestIndex = j;
-    }
-
-    // const temp = arr[i];
-    // arr[i] = arr[smallestIndex];
-    // arr[smallestIndex] = temp;
-
-    // with array destructuring
-    console.log(([arr[i], arr[smallestIndex]] = [arr[smallestIndex], arr[i]]));
-    [arr[i], arr[smallestIndex]] = [arr[smallestIndex], arr[i]];
+const findSmallest = (arr) => {
+  let smallest = arr[0];
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] < smallest) smallest = arr[i];
   }
-  return arr;
+  return arr.indexOf(smallest);
+};
+
+const selectionSort = (arr, newArr = []) => {
+  const smallest = findSmallest(arr);
+  newArr.push(...arr.splice(smallest, 1));
+  if (arr.length !== 0) selectionSort(arr, newArr);
+  return newArr;
 };
 
 console.log(selectionSort(inputArr));
-console.log(inputArr);
 
 // Complexity
 // O(n^2)
